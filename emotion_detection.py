00652b55ef84f6cf821a0d55e0ec4c98282a91c0
gaadha <gaadhajosephm@gmail.com>
2025-02-10 13:04:26 -0500

    Fixed typo in emotion_detector function

--- emotion_detection.py
+++ emotion_detection.py
@@ -4,7 +4,7 @@ import json
 def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    data = {"raw_document": {"text": text_to_analyse}}
+    data = {"raw_document": {"text": text_to_analyze}}
     
     response = requests.post(URL, json=data, headers=headers)
     response_data = json.loads(response.text)
