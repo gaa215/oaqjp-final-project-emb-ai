1f8c67cbc9fa28eb4f6789e1d4c994374c0e67a0
gaadha <gaadhajosephm@gmail.com>
2025-02-11 03:14:34 -0500

    updated 1

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -2,21 +2,25 @@ import unittest
 from emotion_detection import emotion_detector  # Correct import
 
 class TestEmotionDetection(unittest.TestCase):
-    def test_emotion_detector(self):
-        result_1 = emotion_detector("I am happy this happened")
-        self.assertEqual(result_1['dominant_emotion'], 'joy')
+    def test_emotion_detector_joy(self):
+        result = emotion_detector("I am happy this happened")
+        self.assertEqual(result['dominant_emotion'], 'joy')
 
-        result_2 = emotion_detector("I am very angry.")
-        self.assertEqual(result_2['dominant_emotion'], 'anger')
+    def test_emotion_detector_anger(self):
+        result = emotion_detector("I am very angry.")
+        self.assertEqual(result['dominant_emotion'], 'anger')
 
-        result_3 = emotion_detector("I am feeling disgusted here")
-        self.assertEqual(result_3['dominant_emotion'], 'disgust')
+    def test_emotion_detector_disgust(self):
+        result = emotion_detector("I am feeling disgusted here")
+        self.assertEqual(result['dominant_emotion'], 'disgust')
         
-        result_4 = emotion_detector("I am feeling sad for it")
-        self.assertEqual(result_4['dominant_emotion'], 'sadness')
+    def test_emotion_detector_sadness(self):
+        result = emotion_detector("I am feeling sad for it")
+        self.assertEqual(result['dominant_emotion'], 'sadness')
 
-        result_5 = emotion_detector("I have fear if it is not completed.")
-        self.assertEqual(result_5['dominant_emotion'], 'fear')
+    def test_emotion_detector_fear(self):
+        result = emotion_detector("I have fear if it is not completed.")
+        self.assertEqual(result['dominant_emotion'], 'fear')
 
 if __name__ == "__main__":
     unittest.main()
